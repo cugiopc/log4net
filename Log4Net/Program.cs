1ef0385b47a3e0a6fa33eb8db6d6df342a466b7d
Hoan Tran <hpcisme@gmail.com>
2016-10-05 00:01:18 +0700

    add all file

---
+++ Log4Net/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using System.Threading;
+
+namespace Log4Net
+{
+    class Program
+    {
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(Program));
+
+        static void Main(string[] args)
+        {
+            Logger.Debug("Entry main...");
+
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(500);
+                Logger.DebugFormat("Value of I = {0}", i);               
+            }
+            
+            Logger.Debug("Exit main...");
+            Console.ReadLine();
+        }
+    }
+}
